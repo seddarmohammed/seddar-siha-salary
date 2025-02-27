010ed4552f0ed7f3c721dd6b7629111f301b8a82
seddar <ephmaghni@gmail.com>
2025-02-27 01:24:51 +0100

    change 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,15 @@ export default function Home() {
         <main className="flex flex-col items-center justify-center px-4 py-24">
           <div className="max-w-4xl w-full space-y-16">
             <div className="text-center space-y-6">
-              <h1 className="text-4xl sm:text-6xl font-bold">
-                مرحباً بك في منصة رواتب موظفي الصحة
-              </h1>
-              <p className="text-xl text-muted-foreground">
+              <h1 className="text-4xl sm:text-6xl font-bold">مرحباً بك</h1>
+              <p className="text-sm text-muted-foreground">
                 في خِضَمِّ النقاشات العامة حول أجور العاملين في قطاع الصحة، يطفو
                 على السطح إشكالٌ كبير: عجز الإعلام عن تقديم صورة واضحة وعادلة عن
                 أنظمة التعويضات، حيث تُختزل الحقائق في عناوين مُضللة أو أرقام
                 مجتزأة، مما يُغذّي الجدل ويُهمّش جوهر القضية. أمام هذا العرض
                 الباهت وتداول رواتب موظفي الصحة على كل لسان، جاءت فكرة
-                محاولةانشاء منصةٍ تُعيد الحق في معرفة المستحقات تشويه، وتضع بين
-                أيديكم بيانات دقيقة وشفافة،
+                محاولةانشاء منصةٍ تُعيد الحق في معرفة المستحقات وتعطي فهما اوضح
+                للزيادات الاسمية.
               </p>
             </div>
 
